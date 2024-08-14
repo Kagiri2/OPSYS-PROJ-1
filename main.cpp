bf76fc3118769db0cadd635b13750d2422c63b36
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2024-08-14 15:46:00 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -581,7 +581,7 @@ void print_processes(const std::vector<Process>& processes, Totaller& tot) {
                             std::cout << "time " << current_time + 1 << "ms: Recalculated tau for process " << current_process->get_pid() << ": old tau " << old_tau << "ms ==> new tau " << current_process->get_tau() << "ms [Q " << print_queue(ready_queue) << "]" << std::endl;
                         }
 
-                        // int io_time = current_process->start_io(current_time + 3);
+                        current_process->start_io(current_time + 3);
                         if(current_time < 10000) {
                             std::cout << "time " << current_time + 1 << "ms: Process " << current_process->get_pid() 
                                 << " switching out of CPU; blocking on I/O until time " 
@@ -865,7 +865,7 @@ int main(int argc, char** argv) {
     
     std::cout << "\n<<< PROJECT PART II" << std::endl;
     std::cout << std::fixed << std::setprecision(2) << "<<< -- t_cs=" << t_cs << "ms; alpha=" << alpha << "; t_slice=" << t_slice << "ms" << std::endl;
-    
+
     print_algorithm_start("FCFS");
     simulate_fcfs(processes, t_cs);
     print_algorithm_end("FCFS");
@@ -875,7 +875,7 @@ int main(int argc, char** argv) {
     simulate_sjf(processes, t_cs, alpha, lambda);
     print_algorithm_end("SJF");
     reset_processes(processes);
-    
+
     print_algorithm_start("SRT");
     simulate_srt(processes, t_cs, alpha, lambda);
     print_algorithm_end("SRT");
